6bb0b5269f0f709a12dbffc59152ec884382617b
anulocaulis <mike.beitner@ucdenver.edu>
2026-03-05 13:18:37 -0700

    Add comments for event scheduling system structure
    Added comments outlining the structure for event scheduling features.

--- src/linked-list.py
+++ src/linked-list.py
@@ -1,1 +1,16 @@
 ### linked list implementation of campus event scheduling system
+
+
+### Store events (call event-creator.py)
+
+### Sort events
+# Quick sort
+
+# Merge sort
+
+# Insertion sort
+
+### Search events
+# Linear search
+
+# Binary search
