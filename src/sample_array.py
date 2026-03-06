47bea4b2431f9a4ffabf456a8d44052cc075590d
Bockslunch <155188366+Bockslunch@users.noreply.github.com>
2026-03-06 10:16:07 -0700

    changed star to stars to avoid error

--- src/sample_array.py
+++ src/sample_array.py
@@ -7,5 +7,5 @@ hhour = Event(2, "Bites & Beers", "2026-03-06", "16:30", "Avery")
 play = Event(3, "Play & Pals", "2026-03-06", "18:30", "Mary Rippon Outdoor Theatre")
 stars = Event(4, "Stars & Stares", "2026-03-06", "22:00", "Flagstaff")
 
-friday = [brunch, lunch, hhour, play, star]
+friday = [brunch, lunch, hhour, play, stars]
 
